d8a6dea65fc075ae4275f24472ca3f393640badd
ahawk32 <65329230+ahawk32@users.noreply.github.com>
2020-07-13 22:19:38 -0400

    Add files via upload
    init

---
+++ AdvancedSwift/ComputedProperties.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+
+
+let pizzaInInches: Int = 8
+
+//var numberOfSlices: Int = 10
+
+var numberOfSlices: Int {
+    
+    get {
+    return pizzaInInches - 4
+    }
+}
+
+
+//
+//func calcPizzaSlices() {
+//    numberOfSlices = pizzaInInches - 4
+//}
+//
+////let a = numberOfSlices * 2
+//
+//calcPizzaSlices()
+print(numberOfSlices)
